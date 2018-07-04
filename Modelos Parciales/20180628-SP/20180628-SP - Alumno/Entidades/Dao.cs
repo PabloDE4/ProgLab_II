336ff36e8a5fd6a8e91bc55d51582e39d26d2d2d
Pablo <diazechevestepablo@gmail.com>
2018-07-03 21:02:12 -0300

    Modelo segundo parcial
    Votaciones

---
+++ Modelos Parciales/20180628-SP/20180628-SP - Alumno/Entidades/Dao.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using Excepciones;
+
+namespace Entidades
+{
+    public class Dao : IArchivos<Votacion>
+    {
+        public static SqlConnection cadenaConexion;
+        public static SqlCommand cadenaComando;
+        public static SqlDataReader cadenaDataReader;
+        
+
+        public Votacion Leer(string nombre)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Guardar(string nombre, Votacion objeto)
+        {
+            bool retorno = false;
+            try
+            {
+                cadenaConexion = new SqlConnection(Properties.Settings.Default.conexion);
+                cadenaConexion.Open();
+                cadenaComando = new SqlCommand("INSERT INTO " + nombre + " (nombreLey, afirmativos, negativos, abstenciones, nombreAlumno) VALUES ('" + objeto.NombreLey + "','" + objeto.Afirmativo + "','" + objeto.Negativo + "','" + objeto.Abstencion + "','" + "PabloDE" + "')", cadenaConexion);
+                cadenaComando.ExecuteNonQuery();
+                retorno = true;
+            }
+            catch (Exception e)
+            {
+                throw new ErrorArchivoException("Error al guardar en la base de datos", e);
+            }
+            finally
+            {
+                if (retorno)
+                Dao.cadenaConexion.Close();
+            }
+
+            return retorno;
+        }
+    }
+}
---
+++ Modelos Parciales/20180628-SP/20180628-SP - Alumno/Entidades/SerializarBinaria.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Excepciones;
+
+namespace Entidades
+{
+    public class SerializarBinaria : IArchivos<Votacion>
+    {
+        public Votacion Leer(string nombre)
+        {
+            try
+            {
+                Votacion votacionLeida;
+
+                FileStream fs;                 
+                BinaryFormatter ser;      
+                fs = new FileStream(nombre, FileMode.Open);
+
+                ser = new BinaryFormatter();
+
+                votacionLeida = (Votacion)ser.Deserialize(fs);
+
+                fs.Close();
+                return votacionLeida;
+            }
+            catch (Exception e)
+            {
+                throw new ErrorArchivoException("Error al leer el archivo",e);
+            }
+        }
+
+        public bool Guardar(string nombre, Votacion objeto)
+        {
+            try
+            {
+                FileStream fs;  	 
+                BinaryFormatter ser;  
+
+                fs = new FileStream(nombre, FileMode.Create);
+                ser = new BinaryFormatter();
+                ser.Serialize(fs, objeto);
+                fs.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+                throw new ErrorArchivoException("Error al intentar guardar el archivo", e);
+            }
+        }
+
+        
+    }
+}
